c6f90868eba40c1c75350f8677184efc02b76ff5
Nimishms <83498563+Nimishms@users.noreply.github.com>
2021-08-30 16:02:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+class Script{
+constructor(){
+this.button = createButton("Play");
+
+}
+
+display(){
+    background(scriptImg)
+    this.button.position(displayWidth/2,displayHeight-200)
+this.button.mousePressed(()=>{
+this.button.hide();
+game.play();
+    
+
+})
+}
+}
